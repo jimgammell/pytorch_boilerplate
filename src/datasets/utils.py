299d6671b24d47a769bed6487c9bde6ec43a371b
Jimmy Gammell <jgammell@purdue.edu>
2025-03-20 13:48:26 -0400

    ...

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -16,7 +16,6 @@ def get_trace_sample_stats(_traces, cache_path, indices=None, chunk_size=100):
     else:
         worker_id = worker_info.id
     if not(os.path.exists(cache_path)) and worker_id <= 0:
-        traces = np.array(_traces)
         if isinstance(traces, torch.Tensor):
             traces = traces.numpy()
         trace_count = len(indices)
@@ -26,12 +25,12 @@ def get_trace_sample_stats(_traces, cache_path, indices=None, chunk_size=100):
         count = 0
         progress_bar = tqdm(total=2*trace_count)
         for chunk_idx in range(trace_count//chunk_size):
-            traces_chunk = np.array(traces[indices[chunk_idx*chunk_size:(chunk_idx+1)*chunk_size], :]).astype(np.float32)
+            traces_chunk = np.array(traces[chunk_idx*chunk_size:(chunk_idx+1)*chunk_size, :]).astype(np.float32)
             mean = (count/(count+chunk_size))*mean + (chunk_size/(count+chunk_size))*traces_chunk.mean(axis=0)
             count += chunk_size
             progress_bar.update(chunk_size)
         for chunk_idx in range(trace_count//chunk_size):
-            traces_chunk = np.array(traces[indices[chunk_idx*chunk_size:(chunk_idx+1)*chunk_size], :]).astype(np.float32)
+            traces_chunk = np.array(traces[chunk_idx*chunk_size:(chunk_idx+1)*chunk_size, :]).astype(np.float32)
             var = (count/(count+chunk_size))*var + (chunk_size/(count+chunk_size))*((traces_chunk-mean)**2).mean(axis=0)
             count += chunk_size
             progress_bar.update(chunk_size)
