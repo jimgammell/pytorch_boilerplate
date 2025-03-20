544cb81713a05ef9d1561a1c066de8fbc7cbaf43
Jimmy Gammell <jgammell@purdue.edu>
2025-03-20 13:46:11 -0400

    ...

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -16,8 +16,7 @@ def get_trace_sample_stats(_traces, cache_path, indices=None, chunk_size=100):
     else:
         worker_id = worker_info.id
     if not(os.path.exists(cache_path)) and worker_id <= 0:
-        print('Producing the stats cache...')
-        traces = _traces
+        traces = np.array(_traces)
         if isinstance(traces, torch.Tensor):
             traces = traces.numpy()
         trace_count = len(indices)
